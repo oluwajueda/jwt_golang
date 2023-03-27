34dfad23b5ab8c31d11027ee7605dada47569842
oluwajueda <oluwajuedaloade@gmail.com>
2023-03-27 02:48:21 +0100

    final commit

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -53,6 +54,37 @@ if err != nil {
 }
 
 return token, refreshToken, err
+}
+
+
+
+func ValidateToken(signedToken string) (claims *SignedDetails, msg string){
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&SignedDetails{},
+		func(token *jwt.Token)(interface{}, error){
+			return []byte(SECRET_KEY), nil
+		},
+	)
+
+	if err != nil {
+		msg=err.Error()
+		return
+	}
+
+	claims, ok:= token.Claims.(*SignedDetails)
+	if !ok{
+		msg = fmt.Sprintf("the token is invalid")
+		msg = err.Error()
+		return
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix(){
+		msg = fmt.Sprintf("token is expired")
+		msg = err.Error()
+		return
+	}
+	return claims, msg
 }
 
 
